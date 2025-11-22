e5b4a9313d12b2afd526b19b5d73a9a37e1e1b78
sergiotech234 <sergio.herbla.4@educa.jcyl.es>
2025-11-22 12:07:14 +0100

    Update Main.java
    Introduccion de comentarios

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Calculadora calculadora = new Calculadora();
-
+//Utilizamos la excepcion try para que aparezcan los erroes
         try{
             System.out.println("Resultado: " +calculadora.calcular(10,5,"Suma"));
             System.out.println("Resultado: " +calculadora.calcular(10,5,"Resta"));
